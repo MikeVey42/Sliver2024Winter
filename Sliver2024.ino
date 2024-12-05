3c83e26574a82aaf09eea1aaa4eb5f74ad586730
Gabriel Steele <93623975+MikeVey42@users.noreply.github.com>
2024-12-04 21:17:40 -0500

    code ready for basic Pestolink test

--- Sliver2024.ino
+++ Sliver2024.ino
@@ -1,10 +1,11 @@
 #include <PestoLink-Receive.h>
 #include <Alfredo_NoU3.h>
+#include <Keys.h>
 
 // KEYBINDS
-Key startAutoKey = Key.Digit0;
-Key shootKey = Key.I;
-Key intakeKey = Key.J;
+Key startAutoKey = Key::Digit0;
+Key shootKey = Key::I;
+Key intakeKey = Key::J;
 
 // Drivetrain Motors
 NoU_Motor frontLeftMotor(1);
@@ -13,21 +14,23 @@ NoU_Motor rearLeftMotor(3);
 NoU_Motor rearRightMotor(4);
 
 // Flywheels: for launching notes
-NoU_Motor leftFlywheel(5);
-NoU_Motor rightFlywheel(6);
+// NoU_Motor leftFlywheel(5);
+// NoU_Motor rightFlywheel(6);
 
 // Intake: for picking up notes
-NoU_Motor intakeMotor(7);
+// NoU_Motor intakeMotor(7);
 
 // Aiming: these are for aiming the shooter left/right and up/down
 NoU_Servo xAlignServo(1);
-float minimumAngle = 0;
-float maximumAngle = 180;
-float startAngle = 180;
-NoU_Servo yAlignServo(2);
+    float xStartAngle = 180;
+// NoU_Servo yAlignServo(2);
+//     float yStowAngle = 60;
 
 // Sensor Servo: this is for changing the angle of the distance sensor to be horizontal with the ground 
-NoU_Servo distanceSensorServo(3);
+// NoU_Servo distanceSensorServo(3);
+//     float sensorStowAngle = 90;
+//     float sensorMeasureAngle = 90;
+//     float sensorAutoAngle = 90;
 
 // The Drivetrain object handles the arcade drive math for us
 NoU_Drivetrain drivetrain(&frontLeftMotor, &frontRightMotor, &rearLeftMotor, &rearRightMotor);
@@ -64,15 +67,15 @@ void loop() {
     // Here we decide what the servo angle will be based on if button 0 is pressed
     int servoAngle = 0;
 
-    if (PestoLink.buttonHeld(0)) {
-        servoAngle = 70;
+    if (PestoLink.keyHeld(shootKey)) {
+        servoAngle = 0;
     }
     else {
-        servoAngle = 110;
+        servoAngle = 180;
     }
 
     // Here we set the drivetrain motor speeds and servo angle based on what we found in the above code
-    servo.write(servoAngle);
+    xAlignServo.write(servoAngle);
 
     // No need to mess with this code
     PestoLink.update();
@@ -87,8 +90,50 @@ void drive() {
   drivetrain.arcadeDrive(throttle, rotation);
 }
 
+// Returns the yaw in degrees, CCW positive, from -180 to 180
 float getYaw() {
   // TODO: check if this is the right axis
-  return NoU3.gyroscope_x;
+  return -NoU3.gyroscope_z;
 }
 
+// Moves the shooter to aim directly towards the alliance wall.
+// If the robot is in front of the speaker, this will aim it towards the speaker
+float xAlignWithSpeaker() {
+    float currentYaw = getYaw();
+    float targetAngle = (xStartAngle - currentYaw);
+    if (targetAngle > 0 && targetAngle < 180) {
+        xAlignServo.write(targetAngle);
+        return true;
+    }else {
+        return false;
+    }
+}
+
+// Uses a distance sensor to get the distance from the wall
+float getDistance() {
+    // unimplemented
+    return 0;
+}
+
+// Uses a regression to find the ideal vertical (y) angle for the shooter given a distance
+float getTargetShooterAngle() {
+    // unimplemented
+    float distance = getDistance();
+    return 0;
+}
+
+// void stow() {
+//     yAlignServo.write(yStowAngle);
+//     xAlignServo.write(0)
+//     distanceSensorServo.write(sensorStowAngle);
+// }
+
+// void prepareToMeasure() {
+//     yStowAngle.write(yStowAngle);
+//     distanceSensorServo.write(sensorMeasureAngle);
+// }
+
+// void prepareToShoot() {
+//     float targetAngle = getTargetShooterAngle();
+//     yAlignServo.write(yStowAngle);
+// }
