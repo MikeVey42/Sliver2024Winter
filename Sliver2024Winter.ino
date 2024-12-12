8dd5f2a52a911b8f7725e69e95bc6a4509dca5cc
Gabriel Steele <93623975+MikeVey42@users.noreply.github.com>
2024-12-11 21:08:02 -0500

    fixed distance calculation

--- Sliver2024Winter.ino
+++ Sliver2024Winter.ino
@@ -641,7 +641,7 @@ void doSpinUp(bool manual) {
       startEcho = millis();
     }
     if (echoing && !signal) {
-      float distance = (millis() - startEcho) * .0343;
+      float distance = (millis() - startEcho) * .0343 / 2;
       targetYAngle = getTargetShooterAngle(distance);
       print(distance);
       doneMeasuring = true;
